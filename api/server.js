6e57d75a00594b8b5cea9f4d40427bc2b302825e
Hieu Hoang Ngo <71125098+fantasticroll@users.noreply.github.com>
2023-12-12 23:06:43 +0900

    Add files via upload

--- api/server.js
+++ api/server.js
@@ -1,31 +1,58 @@
-// See https://github.com/typicode/json-server#module
+
+// const jsonServer = require('json-server')
+
+// const server = jsonServer.create()
+
+
+// const fs = require('fs')
+// const path = require('path')
+// const filePath = path.join('products.json')
+// const data = fs.readFileSync(filePath, "utf-8");
+// const db = JSON.parse(data);
+// const router = jsonServer.router(db)
+
+
+// const middlewares = jsonServer.defaults()
+
+// server.use(middlewares)
+
+// server.use(jsonServer.rewriter({
+//     '/api/*': '/$1',
+//     '/blog/:resource/:id/show': '/:resource/:id'
+// }))
+// server.use(router)
+// server.listen(3000, () => {
+//     console.log('JSON Server is running')
+// })
+
+// module.exports = server
+
 const jsonServer = require('json-server')
+const fs = require('fs')
+const path = require('path')
 
 const server = jsonServer.create()
 
-// Uncomment to allow write operations
-const fs = require('fs')
-const path = require('path')
-const filePath = path.join('products.json')
-const data = fs.readFileSync(filePath, "utf-8");
-const db = JSON.parse(data);
-const router = jsonServer.router(db)
+// List of JSON files to combine
+const jsonFiles = ['products.json', 'catogories.json', 'companies.json'];
+const combinedData = jsonFiles.reduce((acc, file) => {
+  const filePath = path.join(__dirname, file);
+  const data = fs.readFileSync(filePath, 'utf-8');
+  const jsonData = JSON.parse(data);
+  return { ...acc, ...jsonData };
+}, {});
 
-// Comment out to allow write operations
-// const router = jsonServer.router('db.json')
-
+const router = jsonServer.router(combinedData)
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-// Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
 
-// Export the Server API
-module.exports = server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`JSON Server is running on port ${PORT}`);
+});
